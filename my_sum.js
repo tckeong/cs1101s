8f0002b60ac25be69c8bb70724642f876b197777
Source Academy User <No public email provided>
2023-08-30 20:53:07 +0800

    Changes made from Source Academy

--- my_sum.js
+++ my_sum.js
@@ -1,10 +1,11 @@
+/*
 function my_sum(n) {
     return n === 1
            ? 2
            : n * (n + 1) + solution(n - 1);
 }
 
-/*
+
 function my_sum(n) {
     return n === 1
            ? n * (n + 1)
@@ -12,4 +13,14 @@ function my_sum(n) {
 }
 */
 
-solution(11);
+function sum(term, a, next, b) {
+    return (a > b) ? 0
+                   : term(a) + sum(term, next(a), next, b);
+}
+
+function my_sum(n) { 
+    return sum(x => x * (x + 1), 1, x => x + 1, n); 
+}
+
+
+my_sum(10);
