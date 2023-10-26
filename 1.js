f4e76c79cb31a4e11e29b99d36df8e7aee615642
Source Academy User <No public email provided>
2023-10-26 16:58:29 +0800

    Changes made from Source Academy

---
+++ 1.js
@@ -0,0 +1,20 @@
+function add_streams(s1, s2) {
+    return is_null(s1)
+        ? s2
+        : is_null(s2)
+            ? s1
+            : pair(head(s1) + head(s2),
+                () => add_streams(stream_tail(s1),
+                    stream_tail(s2)));
+}
+
+function partial_sum(s) {
+    return pair(head(s), () => add_streams(partial_sum(s), stream_tail(s)));
+}
+                
+                
+const x = partial_sum(enum_stream(0, 10));
+                
+for(let i = 0; i < 10; i = i + 1) {
+    display(stream_ref(x, i));
+}
