b0dc882e4418532316138cca0b6812dd853d309a
Source Academy User <No public email provided>
2023-10-31 08:52:51 +0800

    Changes made from Source Academy

---
+++ s11.js
@@ -0,0 +1,16 @@
+function add_stream(s1, s2) {
+    // if(is_null(stream_tail(s1)) || is_null(stream_tail(s2))) {
+    //     return null;
+    // } else {
+    //     return pair(head(s1) + head(s2), () => add_stream(stream_tail(s1), stream_tail(s2)));
+    // }
+    return pair(head(s1) + head(s2), () => add_stream(stream_tail(s1), stream_tail(s2)));
+}
+
+const alt_ones = pair(1, () => pair(-1 * head(alt_ones), alt_ones));
+const zeros = add_stream(alt_one, stream_tail(alt_one));
+const ones = pair(1, () => ones);
+
+for(let i = 0; i < 10; i = i + 1) {
+    display(stream_ref(ones, i));
+}
